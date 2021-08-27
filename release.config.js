936092f34dcdf24af4c70ab18215f7c234f19857
Peng Xiao <pengxiao@outlook.com>
2021-08-28 02:31:48 +0800

    fix: release

---
+++ release.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  branches: ["master"],
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+      },
+    ],
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false,
+      },
+    ],
+    "@semantic-release/git",
+    [
+      "@semantic-release/github",
+      {
+        assets: "logseq-plugin-tabs.zip",
+      },
+    ],
+  ],
+};
