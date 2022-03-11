0947f31d5f99122b4dffa9c2c3f2a647d5f04b17
Peng Xiao <pengxiao@outlook.com>
2022-03-11 15:54:35 +0800

    fix: cursor blink issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { PageTabs } from "./PageTabs";
-import { usePreventFocus, useThemeMode } from "./utils";
+import { useThemeMode } from "./utils";
 
 function App(): JSX.Element {
   const themeMode = useThemeMode();
-  usePreventFocus();
   return (
     <main
       style={{ width: "100vw", height: "100vh" }}
